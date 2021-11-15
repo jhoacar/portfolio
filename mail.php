3e1baf990ba03d3e6e31b435fdeacc895f473dd9
jhoan carrero <63968367+jhoacar@users.noreply.github.com>
2021-11-15 02:04:50 -0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -30,10 +30,37 @@
 		
 		$mailheader = "From: $email";
 		
-		if( mail($recipient, $name, $formcontent, $mailheader) == false ){
-			$data['error'] = 'Sorry, an error occured!';
-		}else{
+		$mail = new PHPMailer();
+		//indico a la clase que use SMTP
+		$mail->IsSMTP();
+		//permite modo debug para ver mensajes de las cosas que van ocurriendo
+		$mail->SMTPDebug = 2;
+		//Debo de hacer autenticación SMTP
+		$mail->SMTPAuth = true;
+		$mail->SMTPSecure = "ssl";
+		//indico el servidor de Gmail para SMTP
+		$mail->Host = "smtp.gmail.com";
+		//indico el puerto que usa Gmail
+		$mail->Port = 465;
+		//indico un usuario / clave de un usuario de gmail
+		$mail->Username = "jhoancarrero123@gmail.com";
+		$mail->Password = "carreropineda";
+		$mail->SetFrom('jhoancarrero123@gmail.com', 'Jhoan Carrero');
+		$mail->AddReplyTo("jhoancarrero123@gmail.com","Jhoan Carrero");
+		$mail->Subject = $subject;
+		$mail->MsgHTML($message);
+		//indico destinatario
+		$mail->AddAddress($email, $name);
+		if(!$mail->Send()) {
+			$data['error'] = "Error al enviar: " . $mail->ErrorInfo;
+		} else {
 			$data['error'] = false;
+		} 
+		
+		//if( mail($recipient, $name, $formcontent, $mailheader) == false ){
+		//	
+		//}else{
+		//	
 		}
 	
 	}
