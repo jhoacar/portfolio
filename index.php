524f6ea187e3116b78426f568bc696754f4e9b24
jhoan carrero <63968367+jhoacar@users.noreply.github.com>
2021-11-15 11:25:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php 
 
-require_once("index.english.html") 
-
+$lang = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+switch($lang) {
+    case "de-DE":
+        break;
+    case "es-ES":
+        require_once("index.spanish.html");
+        break;
+    case "cn-CN":
+        break;
+    case "fr-FR":
+        break;
+    case "en-US":
+        require_once("index.english.html");
+        break;
+    default:
+        require_once("index.english.html");
+        break;
+}
 ?>
